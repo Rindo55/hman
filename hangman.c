795e818db3f57d31bb020ad671c3cfc94f151abb
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-12-11 07:37:42 +0530

    Update hangman.c

--- hangman.c
+++ hangman.c
@@ -13,27 +13,88 @@ char guessedWord[MAX_WORD_LENGTH];
 int incorrectTries = 0;
 
 void displayHangman() {
-    // ... (unchanged)
+    switch (incorrectTries) {
+        case 1:
+            printf("  O\n");
+            break;
+        case 2:
+            printf("  O\n");
+            printf("  |\n");
+            break;
+        case 3:
+            printf("  O\n");
+            printf(" /|\n");
+            break;
+        case 4:
+            printf("  O\n");
+            printf(" /|\\\n");
+            break;
+        case 5:
+            printf("  O\n");
+            printf(" /|\\\n");
+            printf(" /\n");
+            break;
+        case 6:
+            printf("  O\n");
+            printf(" /|\\\n");
+            printf(" / \\\n");
+            break;
+        default:
+            break;
+    }
 }
 
 void initializeGame() {
-    // ... (unchanged)
+    const char* words[] = {"hangman", "programming", "interface", "mongoose", "example"};
+    srand(time(NULL));
+    strcpy(secretWord, words[rand() % (sizeof(words) / sizeof(words[0]))]);
+
+    for (int i = 0; i < strlen(secretWord); i++) {
+        guessedWord[i] = '_';
+    }
+    guessedWord[strlen(secretWord)] = '\0';
+
+    incorrectTries = 0;
 }
 
 int makeGuess(char letter) {
-    // ... (unchanged)
+    int correctGuess = 0;
+
+    for (int i = 0; i < strlen(secretWord); i++) {
+        if (secretWord[i] == letter) {
+            guessedWord[i] = letter;
+            correctGuess = 1;
+        }
+    }
+
+    if (!correctGuess) {
+        incorrectTries++;
+    }
+
+    return correctGuess;
 }
 
 int isGameWon() {
-    // ... (unchanged)
+    return strcmp(secretWord, guessedWord) == 0;
 }
 
 int isGameOver() {
-    // ... (unchanged)
+    return incorrectTries >= MAX_TRIES || isGameWon();
 }
 
 static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
-    // ... (unchanged)
+    if (ev == MG_EV_HTTP_MSG) {
+        struct mg_http_message *hm = (struct mg_http_message *)ev_data;
+
+        if (mg_http_match_uri(hm, "/api/hi")) {
+            mg_http_reply(c, 200, "", "{%.*s:%.*s}\n",
+                          (int)hm->uri.len, hm->uri.ptr,
+                          (int)hm->body.len, hm->body.ptr);
+        } else {
+            struct mg_http_serve_opts opts = {.root_dir = "."};
+            mg_http_serve_dir(c, hm, &opts);
+        }
+    }
 }
 
 int main() {
@@ -74,7 +135,7 @@ int main() {
     // Mongoose setup and event loop
     struct mg_mgr mgr;
     mg_mgr_init(&mgr);
-    mg_log_set(MG_LL_DEBUG);
+    mg_log_set("2");  // Set log level to debug
     mg_http_listen(&mgr, "http://0.0.0.0:8000", fn, NULL);
     for (;;) mg_mgr_poll(&mgr, 1000);
 
