982164b7223dae3f37b6608fe359b301e0ce9fa6
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-12-11 07:52:46 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,8 @@
 #include "mongoose.h"
 
-static const char *s_http_port = "8000";
-static struct mg_serve_http_opts s_http_server_opts;
-
-static void ev_handler(struct mg_connection *nc, int ev, void *p) {
+static void ev_handler(struct mg_connection *nc, int ev, void *ev_data) {
     if (ev == MG_EV_HTTP_REQUEST) {
-        mg_serve_http(nc, (struct http_message *) p, s_http_server_opts);
+        mg_http_send_response(nc, 200, "Content-Type: text/plain", "Hello, World!");
     }
 }
 
@@ -14,20 +11,12 @@ int main(void) {
     struct mg_connection *nc;
 
     mg_mgr_init(&mgr, NULL);
-
-    printf("Starting web server on port %s\n", s_http_port);
-    nc = mg_bind(&mgr, s_http_port, ev_handler);
-    if (nc == NULL) {
-        printf("Failed to create listener\n");
-        return 1;
-    }
-
-    // Set up HTTP server options
+    nc = mg_bind(&mgr, ":8080", ev_handler);
     mg_set_protocol_http_websocket(nc);
-    s_http_server_opts.document_root = ".";  // Serve current directory
-    s_http_server_opts.enable_directory_listing = "yes";
 
-    for (;;) {
+    printf("Starting web server on port %s\n", mg_get_option(&mgr, "listening_port"));
+
+    while (1) {
         mg_mgr_poll(&mgr, 1000);
     }
 
